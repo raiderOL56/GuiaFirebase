2bfdadce86d5de3ca9cfdce79cef3c92f45ef14f
raiderOL56 <erick_h98@outlook.com>
2021-10-23 01:19:56 -0500

    Principal COMPLETO: Cada botón lleva al Activity correspondiente

--- app/src/main/java/com/example/firebase/Principal.java
+++ app/src/main/java/com/example/firebase/Principal.java
@@ -2,13 +2,59 @@ package com.example.firebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Principal extends AppCompatActivity {
+
+    // Elementos UI
+    private Button principal_BTNshow, principal_BTNupdate, principal_BTNdelete, principal_BTNsignOut;
+
+    // FIREBASE
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        principal_BTNshow = findViewById(R.id.principal_BTNshow);
+        principal_BTNupdate = findViewById(R.id.principal_BTNupdate);
+        principal_BTNdelete = findViewById(R.id.principal_BTNdelete);
+        principal_BTNsignOut = findViewById(R.id.principal_BTNsignOut);
+
+        principal_BTNshow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Principal.this, Read.class));
+            }
+        });
+
+        principal_BTNupdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Principal.this, Update.class));
+            }
+        });
+
+        principal_BTNdelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Principal.this, Delete.class));
+            }
+        });
+
+        principal_BTNsignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(Principal.this, SignUp.class));
+                finish();
+            }
+        });
     }
 }
